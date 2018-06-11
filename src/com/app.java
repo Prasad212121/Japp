796bd348e4535e66f1a92ba1535da016997a5871
user1 <user1@accenture.com>
2018-06-11 14:38:12 +0530

    updated HDC2

--- src/com/app.java
+++ src/com/app.java
@@ -5,7 +5,7 @@ public class app {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello World-HDC2");
 		System.out.println("Hello World");
 		System.out.println("Hello World1");
 
